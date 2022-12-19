e61ee710c2098b8bf71806978ed58d1f83b7d1db
Justin Meyer <justinbmeyer@gmail.com>
2022-12-18 21:53:20 -0600

    able to serve prod and dev spas

---
+++ serve/prod-spa.js
@@ -0,0 +1,42 @@
+const express = require("express")
+const path = require("path")
+
+
+const app = express()
+
+
+// app things
+app.use("/", express.static(path.join(__dirname, "..","prod")))
+
+
+
+app.use("*", (req, res) => {
+	res.send(makePage(req,res))
+});
+
+app.listen(process.env.PORT || 8080, function () {
+  console.log("Example app listening on port ", process.env.PORT || 8080 || 8080)
+})
+
+
+function makePage(req, res) {
+	return `
+	<html>
+		<head>
+			<link rel="stylesheet" href="/bundles/can-steal-ssg/app/app.css">
+			<script>
+				steal = {
+					paths: {
+						"bundles/*": "bundles/*.js",
+						"bundles/*.css": "bundles/*css"
+					}
+				};
+			</script>
+		</head>
+		<body>
+			<script src="/bundles/can-steal-ssg/app/app.js"></script>
+		</body>
+	</html>
+
+	`
+}
